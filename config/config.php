49cfad1bb4121d121cbabd6ba7ae0f8c64301e38
raymondronald2d <raymondronald2d@gmail.com>
2025-11-09 23:03:38 +0100

    Update config.php

--- config/config.php
+++ config/config.php
@@ -3,24 +3,22 @@
 declare(strict_types=1);
 
 return [
-'app_name' => 'GLSS Global Logistics Security Company',
+    'app_name' => 'GLSS Global Logistics Security Company',
     'admin' => [
         'username' => 'admin',
-        // Password hash for "changeme123!"
-        'password_hash' => '$2y$12$Nww/ZblRVHtA6qDNBII19uGhR75abeIHEwES6Ft3c36basKFQny8O',
+        // Hash for password: Admin123
+        'password_hash' => '$2y$12$fYRDHSYTIx4pVOnka53BeuvNGHyU5Ura9kNw2xWX./lP0hLv5NAEK',
     ],
     'database' => [
         'driver' => 'mysql',
-        'host' => '127.0.0.1',
-        'port' => 3306,
-        'name' => 'swiftship',
-        'username' => 'root',
-        'password' => '',
+        'host' => 'db.pxxl.pro',
+        'port' => 39764,
+        'name' => 'db_2674e1fe',
+        'username' => 'user_921b9fc3',
+        'password' => '8ff5eafca9519bedda238fdfa0129f8d',
     ],
     'tracking' => [
         'prefix' => 'GLS',
         'length' => 8,
     ],
 ];
-
-
